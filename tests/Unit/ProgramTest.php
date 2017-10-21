10725383a6fb8b6071f35efd46f67d966f2ccbf7
Jean-Philippe Léveillé <jpleveille@gmail.com>
2017-10-21 15:14:36 -0400

    Completed program's unit tests

--- tests/Unit/ProgramTest.php
+++ tests/Unit/ProgramTest.php
@@ -2,12 +2,39 @@
 
 namespace Webgraphe\Phlip\Tests\Unit;
 
-use Webgraphe\Phlip\Tests\TestCase;
+use Webgraphe\Phlip\Atom\NumberAtom;
+use Webgraphe\Phlip\Context;
 use Webgraphe\Phlip\Exception\ProgramException;
+use Webgraphe\Phlip\FormCollection\ProperList;
 use Webgraphe\Phlip\Program;
+use Webgraphe\Phlip\Tests\TestCase;
 
 class ProgramTest extends TestCase
 {
+    public function testProgram()
+    {
+        $program = new Program(
+            new ProperList(
+                NumberAtom::fromString('1'),
+                NumberAtom::fromString('2'),
+                NumberAtom::fromString('3')
+            )
+        );
+        $this->assertEquals(3, $program->execute(new Context));
+    }
+
+    public function testParse()
+    {
+        $this->assertEquals(3, Program::parse('1 2 3')->execute(new Context));
+    }
+
+    public function testParseFile()
+    {
+        $file = __DIR__ . '/ProgramTest.phlip';
+        $program = Program::parseFile($file);
+        $this->assertEquals(file_get_contents($file), '"' . $program->execute(new Context) . '"');
+    }
+
     public function testParseNonExistentFile()
     {
         $this->expectException(ProgramException::class);
